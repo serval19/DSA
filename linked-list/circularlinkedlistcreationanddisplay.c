3fd6016934b73710224c709133ba45d774e1f4e6
Diwa Tomy <diwatomy04@gmail.com>
2025-02-07 00:18:36 +0530

    new

---
+++ linked-list/circularlinkedlistcreationanddisplay.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+}
+void main(){
+    int choice=1;
+    int data;
+    stuct node *head,*newnode,*temp;
+    head=0;
+    while(choice){
+        newnode=(struct node *)malloc(sizeof(stuct node));
+        printf("Enter data:");
+        scanf("%d",&newnode->data);
+        newnode->next=0;
+        if(head==0){
+            head=temp=newnode;
+
+        }
+        else{
+            temp->next=newnode;
+            temp=newnode;
+            temp->next=head;
+        }
+        /* displaying the contents*/
+        if(head==0){
+            printf("The list is empty")
+        }
+        else{
+            temp=head;
+            while(temp->next!=head){
+                printf("%d",&temp->data);
+                temp=temp->next;
+            }
+            printf("%d",temp->next);
+        }
+    }
+}
