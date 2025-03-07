b7ad4629f17b42a007d18c7abcf211665a16be16
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-03-08 00:57:54 +0530

    new

---
+++ queue/queueusinglinkedlist.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node *next;  
+};
+struct node *front=0;
+struct node *rear=0;
+void enqueue(int x){
+    struct node *newnode;
+    newnode=(struct node *)malloc(sizeof(struct node));
+    newnode->data=x;
+    newnode->next=0;
+    if(front==0 && rear==0){
+        front=rear=newnode;
+    }
+    else{
+        rear->next=newnode;
+        rear=newnode;
+    }
+}
+void display(){
+    struct node *temp;
+    if(front==0 && rear==0){
+        //empty
+    }
+    else{
+        temp=front;
+        while(temp!=0){
+            printf('%d',temp->data);
+            temp=temp->next;
+        }
+    }
+}
+void dequeue(){
+    struct node *temp;
+    temp=front;
+    if(front==0 && rear==0){
+        //empty
+    }
+    else{
+        printf("%d",front->data);
+        front=front->next;
+        free(temp);
+    }
+}
+void peek(){
+    if(front==0 && rear==0){
+        //empty
+    }
+    else{
+        printf("%d",front->data);
+    }
+}
