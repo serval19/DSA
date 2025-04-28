020e85847cfc4f64d3a862e35287cc2a57ced40f
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-04-28 20:56:34 +0530

    Create linearsearch.c

---
+++ searchingalgo/linearsearch.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+    int a[7]={1,2,3,4,5,6,7};
+    int b=0;
+    for(int i=0;i<8;i++){
+        if(a[i]==data){
+            printf("element found at index %d",i);
+            b=1
+        }
+    }
+    if(b==0){
+        printf("element not found in the array")
+    }
+    return 0;
+}
