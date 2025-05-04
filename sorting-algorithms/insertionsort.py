85c1096bb2a96c80ee862242bf79fe8939ca843e
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-05-04 18:57:07 +0530

    Create insertionsort.py

---
+++ sorting-algorithms/insertionsort.py
@@ -0,0 +1,10 @@
+def insertionsort(arr):
+    for i in range(1,len(arr)):
+        temp=arr[i]
+        j=i-1
+        while j>=0 and arr[j]>temp:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=temp
+    return arr
+print(insertionsort([1,3,56,3,4,13]))
